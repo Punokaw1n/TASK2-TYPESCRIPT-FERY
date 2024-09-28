369215c7889a4dafc73984d3e0ea1bf9b8350160
Fery Yulia Rahman <feryyuliarahman11@gmail.com>
2024-09-28 11:59:49 +0700

    add variable tugas 1

--- tugas1.ts
+++ tugas1.ts
@@ -1,110 +1,35 @@
 //! STRING
 let nama: string = "Fery Yulia Rahman";
+let namaPanggilan: string = "Fery";
+let tempatBerproses: string = "Product Zilla Academy";
 const alamat: string = "Surabaya Jawa timur";
+let tempatLahir: string = "Lumajang Jawa timur";
+let hewanFavorite: string = "kuda nil";
+let namaLatin: string = "Hippoputamuss Hippopotamus amphibius";
+let speedHippo: string = "30 km/jam";
+let tempatWisata: string = "Tugu Pahlawan";
+let stadion: string = "Gelora Bung Tomo";
+let pahlawan: string = "Bung Tomo";
+let bahasaTradisonal: string = "Jawa";
+let bahasaNasional: string = "Indonesia";
 
 //!NUMBER INT
 let age: number = 21;
-
+let tahunLahir: number = 2003;
+let tahunLahirSurabaya: 1293;
+let umurHippo: number = 40;
+let daruratCall: number = 112;
+let tahunLahirPersebaya: number = 1927;
 //! NUMBER FLOAT
 let tinggiBadan: number = 1.65;
+let massaHippo: number = 4.5;
 
 //! BOOLEAN
 let lulus: boolean = false;
-
-//! UNDEFIND
-let pekerjaan: undefined = undefined;
-
-//!NULL
-let nilai: null = null;
-
-//! UNION TYPES
-let value: string | number;
-
-//!TYPE ALIASES
-type StringOrBoolean = string | boolean;
-
-//!SYMBOL
-const uniqueSymbolPassword: symbol = Symbol("myUniqueSymbolPassword");
-
-//! ANY
-let whatever: any = "Ini bisa di isi apa saja";
-
-//! ENUM
-enum Warna {
-  Pink,
-  Hijau,
-  Salmond,
-}
-let warnaFavorit: Warna = Warna.Salmond;
-
-//! Array of Numbers
-let nilaiExamp: number[] = [70, 80, 95];
-
-//! ARRAY OF STRING
-let daftarNamaKu: string[] = ["Fery", "Yulia", "Rahman"];
-
-//! ARRAY OF ANY
-let mix: any[] = [20, "Fery", true];
-
-//!TUPLE
-let koordinat: [number, number] = [10.5, 15.2];
-
-//! UNKNOWN
-let tipeDataTakDiketahui: unknown = "Bisa tipe apa saja";
-
-//! OBJECK
-let dataDiri: { nama: string; umur: number; lulus: boolean } = {
-  nama: "Fery",
-  umur: 20,
-  lulus: false,
-};
-//! INTERFACE
-interface Person {
-  firstName: string;
-  lastName: string;
-  alamat: string;
-  menikah: boolean;
-  age: number;
-}
-
-//! VOID
-function funcVodi(): void {
-  console.log("Pesan telah dicetak. digunakan untuk tanpa return value");
-}
-//! FUNTION TYPE
-let kali: (a: number, b: number) => number = function (a, b) {
-  return a * b;
-};
-//! FUNCTION NEVER
-function errorMessage(message: string): never {
-  throw new Error(message);
-}
-//! LITERAL TYPES
-let arahAngin: "tenggara" | "barat" = "tenggara";
-
-//! READONLY ARRAY
-let daftarBaca: readonly number[] = [1, 2, 3];
-
-//! INTERSECTION TYPE
-type orang = { nama: string };
-type pekerjaan = { pekerjaan: string };
-let orang: orang & pekerjaan = { nama: "Fery", pekerjaan: "Supir Bis" };
-
-//! TYPE ASSERTION
-let panjangAngka: any = "1000";
-let angkaDariString: number = (panjangAngka as string).length;
-
-console.log(nama);
-console.log(orang);
-console.log(angkaDariString);
-console.log(age);
-console.log(tinggiBadan);
-console.log(koordinat);
-console.log(kali(2, 1));
-console.log(errorMessage);
-console.log(" ");
-console.log("FUNCTION OPERASI MATEMATIKA : ");
-console.log(" ");
+let mamalia: boolean = true;
+let karnivora: boolean = false;
+let jawaTengah: boolean = false;
+let ibuKotaJawaTimur: boolean = true;
 
 //?  TUGAS FUNTION OPERASI MATEMATIKA
 //! SAYA MENNGGUNAKAN 2 FUNCTION TYPESCRIPT DAN 2 LAGI ARROW FUNCTION TYPESCRIPT
